60009863d5c36d2b0f2cb257f8a9889368d5d40d
Christian Almli <59100233+chralmli@users.noreply.github.com>
2020-03-29 19:45:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// question 1
+// my answer for question 1
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+};
+
+// question 2
+// my answer for question 2
+const heading = document.querySelector("h3");
+console.dir(heading);
+
+// question 3
+// my answer for question 3
+const heading = document.querySelector("h3");
+heading.style.fontSize = "2em";
+
+// question 4
+// my answer for question 4
+heading.classList.add("subheading");
+console.log(heading.className);
+
+
+// question 5
+// my answer for question 5
+const paragraphs = document.querySelectorAll("p");
+console.dir(paragraphs);
+
+// question 6
+// my answer for question 6
+const resultsContainer = document.getElementsByClassName("results");
+
+resultsContainer.innerHTML = `<p>New paragraph</p>`;
+
+// question 7
+// my answer for question 7
+function catFunc(catArray) {
+    for (let i = 0; i < catArray.length; i++)
+          console.log(catArray[i].name);
+      }
+  
+  catFunc(catArray); {
+      const cats = [
+          {
+              name: "Blob",
+              age: 10
+          },
+          {
+              name: "Harold",
+          },
+          {
+              name: "Blurt",
+              age: 21
+          }
+      ];
+      console.log(catArray[i].name);
+  }
+
+// question 8
+// my answer for question 8
+
+// question 9
+// my answer for question 9
+
+// question 10
+// my answer for question 10
